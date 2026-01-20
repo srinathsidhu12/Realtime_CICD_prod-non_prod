70b252eb3bd031a9a502cabe4660608510dccebf
Ubuntu <ubuntu@ip-172-31-35-237.ap-south-1.compute.internal>
2026-01-20 07:46:10 +0000

    changes made in code

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -16,7 +16,7 @@ public class DemoApplication {
 class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello from Spring Boot! Deployed via Jenkins CI/CD_realtime_new_deployment by addding feature2..🚀";
+        return "Hello from Spring Boot! Deployed via Jenkins CI/CD_realtime_new_deployment by addding feature2_new..🚀";
     }
 }
 
